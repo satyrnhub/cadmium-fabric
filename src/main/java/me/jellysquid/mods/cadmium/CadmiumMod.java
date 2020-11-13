a5d390f069a133cde17e9defbc04f023d832340d
Lucy-t <64509677+Lucy-t@users.noreply.github.com>
2020-11-13 22:09:25 +1030

    use GrossFabricHacks to load the forked DFU

--- src/main/java/me/jellysquid/mods/cadmium/CadmiumMod.java
+++ src/main/java/me/jellysquid/mods/cadmium/CadmiumMod.java
@@ -1,10 +1,13 @@
 package me.jellysquid.mods.cadmium;
 
+import net.devtech.grossfabrichacks.GrossFabricHacks;
+import net.devtech.grossfabrichacks.jarboot.JarBooter;
 import net.fabricmc.api.ModInitializer;
+
 
 public class CadmiumMod implements ModInitializer {
     @Override
     public void onInitialize() {
-
+        JarBooter.addUrl(GrossFabricHacks.class.getResource("/META-INF/jars/datafixerupper-cadmium-4.0.0-SNAPSHOT.jar"));
     }
 }
